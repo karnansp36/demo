944e788098291ea2f2009275f6aae60a5c9572a8
karnansp36 <reviewmaster36@gmail.com>
2025-12-02 12:45:34 +0530

    day-2

---
+++ js/day-2.js
@@ -0,0 +1,136 @@
+// let a = 1;
+// let mark = 10;
+// if (a < 10) {
+//   console.log("a is smaller than 10");
+//   console.log("program executed");
+//   if(mark>10){
+//     console.log('pass')
+//   }
+//   else{
+//     console.log('failed')
+//   }
+// }
+// else if(a<20){
+//       console.log("a is smaller than 20");
+// }
+// else if(a<30){
+//       console.log("a is smaller than 20");
+
+// }
+// else{
+//   console.log('a is greater than 30')  
+// }
+
+// password ='admin@123'
+
+// if(password == 'admin@12'){
+//     console.log('successfully sign in')
+// }
+// else{
+//     console.log('incorrect password')
+// }
+//   console.log("if program executed");
+
+
+
+// lang= 'english'
+// switch(lang){
+//     case 'french':
+//         console.log('it is french language');
+//         break;
+        
+//     case 'tamil':
+//         console.log('நீங்கள் தமிழைத் தேர்ந்தெடுத்துள்ளீர்கள்');
+//         break;
+//     case 'hindi':
+//         console.log('आपने तमिल चुना');
+//         break;
+//     default:
+//         console.log(' select upon three languages')
+
+// }
+
+
+// console.log('successfully switch finished')
+
+// operator='-'
+// a=10
+// b=30
+// switch(operator){
+//     case '+':
+//         console.log(a+b);
+//         break;
+        
+//     case '-':
+//         console.log(a-b);
+//         break;
+//     case '*':
+//         console.log(a*b);
+//         break;
+//     default:
+//         console.log(' select upon three languages')
+
+// }
+
+
+
+// a= 10;
+
+
+
+// for(let i =a;i>=0; i--){
+//     console.log(i)
+//     console.log("loop is running ...")
+
+// }
+
+
+
+
+
+
+
+// for(let i =0;i<=a; i=i+2){
+//     console.log(i)
+//     console.log("loop is running ...")
+
+// }
+
+
+// a= 11;
+// while(a<=10){
+//      console.log(a)
+//      a=a+1
+//     console.log("loop is running ...")
+// }
+
+
+
+
+// a= 5;
+
+// do{
+//      console.log(a)
+//      a=a+1
+//     console.log("loop is running ...")
+// }
+// while(a<=10);
+
+
+
+// let username = prompt('enter the user name');
+// let password = prompt('enter the  password');
+
+// while(password !="admin@123"){
+//    alert('incorrect password')
+//     password = prompt('enter the  password');
+   
+// }
+// alert('signup successfully')
+
+
+
+// function add(){
+//     console.log('function executed')
+// }
+// add()
