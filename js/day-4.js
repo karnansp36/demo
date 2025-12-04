c06ce1b678f59084825535a88e3751ea97aea6c4
karnansp36 <reviewmaster36@gmail.com>
2025-12-04 12:57:32 +0530

    day-4

---
+++ js/day-4.js
@@ -0,0 +1,67 @@
+// let a=`This is jav2ascript`
+
+// console.log(a.replace("is", "python"))
+// console.log(a.replaceAll("is", "python"))
+// console.log(a.split(" "))
+// console.log(a.indexOf('is'))
+// console.log(a.lastIndexOf('is'))
+// console.log(a.search('is'))
+// // console.log(a.match(/Is/i))
+// // let itr =a.matchAll(/[\S]/mgi)
+
+// // for(let x of itr){
+// //     console.log(x)
+// // }
+// console.log(a.includes('python'))
+// console.log(a.startsWith('This'))
+// console.log(a.endsWith('ip'))
+
+
+
+// a = 'javascript'
+// b = 10000
+// // javascript has 10000 followers
+// console.log(a + ' '+ 'has '+b+' followers')
+// // template literals
+
+// console.log(`${a} has ${b} followers`)
+
+
+
+
+// Array
+
+// let a =[34,56,[100,200,[10,20,30],300],5,78]
+
+// console.log(a[2][2][2])
+
+// console.log(a.length)
+// console.log(a.toString(""))
+// console.log(a.at(-3))
+// console.log(a.join(""))
+// console.log(a.pop())
+// console.log(a)
+// a.pop()
+// a.push(300,45,'car')
+// a.shift()
+// a.shift()
+// a.unshift(1000,'bus', true)
+
+// const a=[34,5,67,78]
+// let b =[45, 'bus', 'car']
+// // delete a[1]
+// // let c = a.concat(b)
+// // let d =[...a, 34,56, ...b]  //spread operator
+// console.log(c)  
+// console.log(d)  
+
+
+let c= [10,20,30,40,50]
+// console.log(c.copyWithin(4,2,5))
+// console.log(c.flat(Infinity))
+
+// c.splice(1,0,'box',34)
+console.log(c.toSpliced(1,0,'box',34))
+console.log(c.slice(1,4))
+
+console.log(c)
