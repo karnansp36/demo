12f9f412e6a5464dedf9eb8ca8a0ea6b9233dfa3
karnansp36 <reviewmaster36@gmail.com>
2025-12-11 15:54:31 +0530

    day-9

---
+++ js/day-9.js
@@ -0,0 +1,136 @@
+// // oops - object oriented programming   // inheritance
+
+// class Vehicle{
+//     constructor(name, year, color){
+//         this.name = name
+//         this.year = year
+//         this.color = color
+//     }
+
+//     info(){
+//         return `name is  ${this.name}, year is ${this.year} and color is ${this.color}`
+//     }
+// }
+// class Car extends Vehicle{
+//     display(){
+//         return `this is a car ${this.name}`
+//     } 
+// }
+// class Bus extends Car{
+//     display2(){
+//         return `this is a car ${this.name}`
+//     } 
+// }
+
+// let car = new Car('BMW', 2020, 'red')
+// let car2 = new Vehicle('Benz', 2025, 'black')
+
+// console.log(car2.display())
+
+
+
+// class Car {
+//     constructor(make, model, year) {
+//       this.make2 = make;
+//       let _model = model;
+//       let _year = year;
+  
+//       this.getCarDetails = function() {
+//         return `${this.make2} ${_model}, ${_year}`;
+//       };
+  
+//       this.updateModel = function(newModel) {
+//         _model = newModel;
+//       };
+//     }
+//   }
+//   class Bus extends Car{
+//     drive(){
+//       return `${this.make2} ${this._model}, ${this._year}`;
+//     }
+//   }
+//   const myCar = new Bus('Toyota', 'Corolla', 2020);
+  
+//   console.log(myCar.getCarDetails()); 
+  
+//   // myCar.updateModel('Camry');
+  
+//   // console.log(myCar.getCarDetails()); 
+  
+//   // console.log(myCar.drive()); 
+//   console.log(myCar._model)
+
+
+
+// class CoffeeMachine {
+//     #boilWater() {
+//       console.log("Boiling water...");
+//     }
+  
+//     #grindBeans() {
+//       console.log("Grinding coffee beans...");
+//     }
+  
+//     #brewCoffee() {
+//       console.log("Brewing the coffee...");
+//     }
+  
+//     #pourInCup(a, b) {
+//       return a+b
+//     }
+  
+//     makeCoffee() {
+//       console.log("Boiling water...");
+//       this.#boilWater();
+//       this.#grindBeans();
+//       this.#brewCoffee();
+//       this.#pourInCup(23,45);
+//       console.log("Your coffee is ready!");
+//     }
+//   }
+
+
+//   class tea extends CoffeeMachine{
+     
+//   }
+  
+//   const myCoffeeMachine = new tea();
+  
+// //   myCoffeeMachine.makeCoffee();
+  
+//  console.log(myCoffeeMachine.makeCoffee())
+
+
+
+
+
+
+class Vehicle{
+    constructor(name, year, color){
+        this.name = name
+        this.year = year
+        this.color = color
+    }
+
+    info(){
+        return `name is  ${this.name}, year is ${this.year} and color is ${this.color}`
+    }
+    display(){
+        return `this is a car ${this.name}`
+    } 
+}
+class Car extends Vehicle{
+    display(){
+        return `this is a car child ${this.name}`
+    } 
+}
+class Bus extends Vehicle{
+    display(){
+        return `this is a car child ${this.name}`
+    } 
+}
+
+let car = new Car('BMW', 2020, 'red')
+let car2 = new Vehicle('Benz', 2025, 'black')
+
+console.log(car2.display())
